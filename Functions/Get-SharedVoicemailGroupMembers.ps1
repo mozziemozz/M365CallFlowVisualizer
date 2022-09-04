f2dcfe6f3b3d42607b182f51127c47b3e898c98d
martin heusser <50239556+mozziemozz@users.noreply.github.com>
2022-09-04 19:06:22 +0200

    2.8.0

--- Functions/Get-SharedVoicemailGroupMembers.ps1
+++ Functions/Get-SharedVoicemailGroupMembers.ps1
@@ -1,16 +1,31 @@
+<#
+    .SYNOPSIS
+    Reads group members of an M365 Group.
+    
+    .DESCRIPTION
+    Author:             Martin Heusser
+    Version:            1.0.0
+    Changelog:          .\Changelog.md
+
+#>
+
 function Get-SharedVoicemailGroupMembers {
     param (
-        [Parameter(Mandatory=$false)][String]$Id
+        [Parameter(Mandatory=$false)][String]$SharedVoicemailGroupId
     )
 
-    $sharedVoicemailGroupMembers = Get-MgGroupMember -GroupId $Id
+    $sharedVoicemailGroupMembers = Get-MgGroupMember -GroupId $SharedVoicemailGroupId
+
+    $mdSharedVoicemailGroupMembers = "<br><br>Members"
 
     foreach ($sharedVoicemailGroupMember in $sharedVoicemailGroupMembers) {
 
-        $currentMember = (Get-MgUser -UserId $sharedVoicemailGroupMember.Id).Mail.Replace("@","'AT'")
+        $currentMember = (Get-MgUser -UserId $sharedVoicemailGroupMember.Id).Mail.Replace("@"," at ")
         
-        $currentMember
+        $mdSharedVoicemailGroupMembers += "<br>$currentMember"
 
     }
+
+    #return $mdSharedVoicemailGroupMembers
 
 }
